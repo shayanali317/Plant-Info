f76718b74aa807b3d73fbd2fd5418315e8f44f81
Shayan ali khan <shayanali317@gmail.com>
2023-07-28 08:21:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/plantinfo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.plantinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int splashDelayMillis = 5000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent lottieintent = new Intent(MainActivity.this, Signup.class );
+                startActivity(lottieintent);
+                finish();
+            }
+        }, splashDelayMillis);
+
+    }
+}
